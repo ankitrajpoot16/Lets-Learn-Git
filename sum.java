c2657b24c287382238b7afdf8f77999b1912219b
Ankit Rajput <ankitrajpoot180@gmail.com>
2023-08-03 10:39:45 +0530

    change

--- sum.java
+++ sum.java
@@ -4,7 +4,6 @@ public class sum {
     public static void main(String[] args){
         int a=5;
         int b=6;
- 
-        System.out.println(a+b);
+        System.out.println("Sum of "+a+" and " +b+":");
     }
 }
